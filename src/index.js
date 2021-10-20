5f0c3795bb10fda16538a5c64ca0606c9542a13e
sindrimt <sindrimt@stud.ntnu.no>
2021-10-20 22:34:40 +0200

    adfsgjkdhs

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./global.css";
 
+import { BrowserRouter as Router } from "react-router-dom";
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    ,
+  </Router>,
   document.getElementById("root")
 );
